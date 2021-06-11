9c54ee4f31bc2546191e4f3903fa082e598a6ae1
Barb Cutler <bmcutler@gmail.com>
2021-06-11 03:33:21 -0400

    sysadmin

--- navtreedata.js
+++ navtreedata.js
@@ -76,7 +76,28 @@ var NAVTREE =
         ] ],
         [ "Instructor", "", [
         ] ],
-        [ "System Administrator", "", [
+        [ "System Administrator", "/sysadmin", [
+            [ "Server OS", "/sysadmin", null ],
+            [ "Installation", "/sysadmin/installation", null ],
+            [ "Update Submitty", "/sysadmin/update", null ],
+            [ "Installation Version Notes", "/sysadmin/version_notes", [
+                [ "v21.01.01", "/sysadmin/version_notes/v21.01.01", null ],
+                [ "v20.09.00", "/sysadmin/version_notes/v20.09.00", null ],
+                [ "v20.08.00", "/sysadmin/version_notes/v20.08.00", null ],
+                [ "v19.08.03", "/sysadmin/version_notes/v19.08.03", null ],
+                [ "v19.06.02", "/sysadmin/version_notes/v19.06.02", null ],
+                [ "v.19.06.01", "/sysadmin/version_notes/v.19.06.01", null ]
+            ] ],
+            [ "System Debugging", "/sysadmin/system_debugging", null ],
+            [ "Database Overview", "/sysadmin/database_overview", null ],
+            [ "User Access Level", "/sysadmin/user_access_level", null ],
+            [ "Course Creation", "/sysadmin/course_creation", null ],
+            [ "Setting up Internal Git", "/sysadmin/git", null ],
+            [ "Worker Installation", "/sysadmin/worker_installation", null ],
+            [ "System Customization Checklist", "/sysadmin/system_customization", null ],
+            [ "Email Configuration", "/sysadmin/email_configuration", null ],
+            [ "Course Archiving", "/sysadmin/course_archiving", null ],
+            [ "Rainbow Grades", "/sysadmin/rainbow_grades", null ]
         ] ],
         [ "Developer", "/developer", [
             [ "Overview", "/developer", null ],
@@ -117,7 +138,7 @@ var NAVTREE =
             ] ],
             [ "Interface Design", "/developer/interface_design_style_guide", [
                 [ "Web Accessibility", "/developer/interface_design_style_guide/web_accessibility", null ]
-            ] ],   
+            ] ],
             [ "Developing the PHP Site", "/developer/developing_the_php_site", [
                 [ "Model", "/developer/developing_the_php_site/model", null ],
                 [ "View", "/developer/developing_the_php_site/view", null ],
@@ -147,7 +168,6 @@ var NAVTREE =
         ] ]
     ];
 
-  
 //[ "", "", null ]
 
 
