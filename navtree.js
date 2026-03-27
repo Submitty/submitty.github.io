aedb4d127505a6d1952701f0c7c10bfe8ff7ad15
Rudranshi Mittal <mittal.rudranshi@gmail.com>
2026-03-28 00:10:35 +0530

    [UI/UX] Make entire sidebar item container clickable with hover effect

--- navtree.js
+++ navtree.js
@@ -193,6 +193,12 @@ function newNode(o, po, text, link, childrenData, lastNode)
   node.itemDiv = document.createElement("div");
   node.itemDiv.className = "item";
 
+  node.itemDiv.style.cursor = "pointer";
+  node.itemDiv.onclick = function() {
+    var a = node.itemDiv.querySelector("a");
+    if (a) a.click();
+  };
+
   node.labelSpan = document.createElement("span");
   node.labelSpan.className = "label";
 
